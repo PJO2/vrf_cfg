020176723e3eaf31252ca7ef1b30d06e6f2af6bf
ark <ark@Debian>
2021-04-26 14:51:41 +0200

    add the locator keyword

--- generate.py
+++ generate.py
@@ -13,24 +13,57 @@ import netaddr_filters
 # local files
 import get_cpe_data
 
+# --------------------------------------------
+# Jinja2 hacks  and enlightments
+# --------------------------------------------
 # need to define a user function to reverse parameter
 def json_query(data, expr):
       """Query data into json record using jmespath query language"""
       return jmespath.search(expr, data)
 
-def render_per_vrf_template (tmpl_name, cpe_info, site_vrf_vars):
+def jinja2_env(**kwargs):
+      env = jinja2.Environment(**kwargs)
+      env.filters ['ipaddr'] = netaddr_filters.ipaddr      # add netaddr filters (ansible way)
+      env.filters ['json_query'] = json_query              # add json_query filter (ansible way)
+      return env
+
+
+# --------------------------------------------------------------------------
+# locate specific data inside the cpe centered structure
+# return a iterable list
+# --------------------------------------------------------------------------
+def locate_template_specific_data (template, cpe_info):
+     json_vars = None
+     #  specific data are specified in the template database with the locator keyword
+     if 'locator' in template:
+         env = jinja2_env()
+         template = env.from_string( '{{ ' + template['locator'] + ' }}'  )
+         output = template.render(cpe=cpe_info)
+         json_vars = json.loads ( output.replace("'", '"') )     
+     return json_vars
+
+
+# --------------------------------------------------------------------------
+# execute a template chunk
+# may iterate depending on supp_vars
+# --------------------------------------------------------------------------
+def render_single_template (tmpl_name, cpe_info, supp_vars):
      """ resolve a single template """
      file_loader = jinja2.FileSystemLoader('templates')   # placeholder for jinja2 templates
-     env = jinja2.Environment(loader=file_loader)
-     env.filters ['ipaddr'] = netaddr_filters.ipaddr      # add netaddr filters (ansible way)
-     env.filters ['json_query'] = json_query              # add json_query filter (ansible way)
-     template = env.get_template(tmpl_name + '.j2')       # open jinja2 template
-     output = template.render(cpe=cpe_info, vrf=site_vrf_vars)      # and convert it
-
+     env = jinja2_env(loader=file_loader)
+     template = env.get_template( tmpl_name )             # open jinja2 template
+     if isinstance(supp_vars, list):
+        output = ""
+        for vars in supp_vars:
+            output += template.render(cpe=cpe_info, vars=vars)
+     else:
+            output = template.render(cpe=cpe_info, vars=supp_vars) 
      return output
 
 
+# ---------------------------------------------------------------------------
 # Assign a Create, Update, Delete service to a CPE
+# ---------------------------------------------------------------------------
 def assign_service (service, cpe_info):
      """ generate CPE configuration for a given Service (except read service)
 
@@ -38,13 +71,16 @@ def assign_service (service, cpe_info):
      ouptut: the sums of all templates reltives to the service"""
 
      config = {}
-     for vrf in cpe_info['topology']['vrfs']:
-         for template in vrf['templates']:
-             if service in template['services']:
-                site_vrf_vars = get_cpe_data.find_attribut ('vrfs', vrf['name'], cpe_info['site']['vars'])
-                config[template['name']] = render_per_vrf_template (template['name'], 
-                                                                   cpe_info, 
-                                                                   site_vrf_vars)
+     # parse the template list for the current role
+     for template in cpe_info['role']['templates']:
+          # check the template match the service to be applied
+          if service in template['services']:
+              json_supp_vars = locate_template_specific_data(template, cpe_info)
+              print ("parsing tempate {}, locator is {}\njinja2 render vars is {}".
+                          format( template['name'], template['locator'], json_supp_vars) )
+              config[template['name']] = render_single_template (template['name'], 
+                                                                 cpe_info, 
+                                                                 json_supp_vars)
      return config
 
 
