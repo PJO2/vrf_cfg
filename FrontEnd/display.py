4e600be936cba4e0acc4880403b52368ce78c53f
Ph. Jounin <philippejounin.net>
2021-06-07 18:04:10 +0200

    disable cache

--- FrontEnd/display.py
+++ FrontEnd/display.py
@@ -45,6 +45,18 @@ def site_info(name):
    return flask.render_template('site_info.html', site=site_info)
 
 
+@frontend.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 if __name__ == "__main__":
     # Set up logging
     frontend.logger.setLevel(logging.__getattribute__(config.LOGGING_LEVEL))
